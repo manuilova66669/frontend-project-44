c92174e1dfcd2a879df5a3d471281bbc1990c00f
manuilova66669 <manuilova66669@gmail.com>
2026-04-20 12:42:33 +0300

    Remove extra semicolon

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import { question } from 'readline-sync';
+import { question } from 'readline-sync'
 
-const roundsCount = 3;
+const roundsCount = 3
 
 const runEngine = (description, getRoundData) => {
-  console.log('Welcome to the Brain Games!');
-  const name = question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log(description);
+  console.log('Welcome to the Brain Games!')
+  const name = question('May I have your name? ')
+  console.log(`Hello, ${name}!`)
+  console.log(description)
 
   for (let i = 0; i < roundsCount; i += 1) {
-    const [questionText, correctAnswer] = getRoundData();
-    console.log(`Question: ${questionText}`);
-    const userAnswer = question('Your answer: ');
+    const [questionText, correctAnswer] = getRoundData()
+    console.log(`Question: ${questionText}`)
+    const userAnswer = question('Your answer: ')
 
     if (userAnswer !== String(correctAnswer)) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log(`Let's try again, ${name}!`)
+      return
     }
-    console.log('Correct!');
+    console.log('Correct!')
   }
 
   console.log(`Congratulations, ${name}!`);
