b0a3707cca517d222d9479682ac17369e7cc7f08
manuilova66669 <manuilova66669@gmail.com>
2026-04-20 12:24:20 +0300

    Final fix: all syntax errors in games are resolved

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,19 +1,30 @@
-import runEngine from '../index.js'
-const description = 'What is the result of the expression?'
+import runEngine from '../index.js';
+
+const description = 'What is the result of the expression?';
+
 const getRoundData = () => {
-  const num1 = Math.floor(Math.random() * 20)
-  const num2 = Math.floor(Math.random() * 10)
-  const operators = ['+', '-', '*']
-  const operator = operators[Math.floor(Math.random() * operators.length)]
-  const question = `${num1} ${operator} ${num2}`
-  let result
+  const num1 = Math.floor(Math.random() * 20);
+  const num2 = Math.floor(Math.random() * 10);
+  const operators = ['+', '-', '*'];
+  const operator = operators[Math.floor(Math.random() * operators.length)];
+  const question = `${num1} ${operator} ${num2}`;
+  let result;
   switch (operator) {
-  case '+': result = num1 + num2 break
-  case '-': result = num1 - num2 break
-  case '*': result = num1 * num2 break
-  default: break
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    default:
+      break;
   }
-  const correctAnswer = String(result)
-  return [question, correctAnswer]
-}
-export default () => runEngine(description, getRoundData)
+
+  const correctAnswer = String(result);
+  return [question, correctAnswer];
+};
+
+export default () => runEngine(description, getRoundData);
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,19 +1,20 @@
-import runEngine from '../index.js'
+import runEngine from '../index.js';
 
-const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num < 2) return false
-  for (let i = 2 i <= Math.sqrt(num) i += 1) {
-    if (num % i === 0) return false
+  if (num < 2) return false;
+  // Исправлено: добавлены точки с запятой в условии цикла
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
   }
-  return true
-}
+  return true;
+};
 
-const generateRound = () => {
-  const question = Math.floor(Math.random() * 100) + 1
-  const answer = isPrime(question) ? 'yes' : 'no'
-  return [String(question), answer]
-}
+const getRoundData = () => {
+  const questionText = Math.floor(Math.random() * 100) + 1;
+  const correctAnswer = isPrime(questionText) ? 'yes' : 'no';
+  return [String(questionText), correctAnswer];
+};
 
-export default () => runEngine(description, generateRound)
+export default () => runEngine(description, getRoundData);
